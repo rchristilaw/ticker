d71abfc09e520f99f9ffed3c8739efb20faebf40
Ryan Christilaw <ryan.christilaw@gmail.com>
2018-03-04 12:18:54 -0500

    Tweak led routine

--- rpi-rgb-led-matrix/bindings/python/samples/goalLed.py
+++ rpi-rgb-led-matrix/bindings/python/samples/goalLed.py
@@ -13,7 +13,7 @@ class GoalLed(LedBase):
         font.LoadFont("../../../fonts/9x15.bdf")
         textColor = graphics.Color(0, 0, 255)
         pos = offscreen_canvas.width
-        my_text = "Leafs Score"
+        my_text = "Leafs Score!"
         print "Before While Loop"
 
         max_brightness = self.matrix.brightness
@@ -28,13 +28,15 @@ class GoalLed(LedBase):
                 count += 1
             else:
                 self.matrix.brightness -= 1
-
-        while True:
+        
+        textCount = 0
+        while textCount < 3:
             offscreen_canvas.Clear()
             len = graphics.DrawText(offscreen_canvas, font, pos, 12, textColor, my_text)
             pos -= 1
             if (pos + len < 0):
                 pos = offscreen_canvas.width
+                textCount += 1
 
             time.sleep(0.025)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
--- rpi-rgb-led-matrix/bindings/python/samples/leafs.py
+++ rpi-rgb-led-matrix/bindings/python/samples/leafs.py
@@ -1,36 +1,59 @@
 #!/usr/bin/env python
 # Display a runtext with double-buffering.
 from goalLed import GoalLed
+import urllib2
+import json
+import time
+import requests
+
+
+
+def getAndParseData(url, goal_led):
+
+    r = requests.get(url)
+    teamdata = r.json()
+    # content = urllib2.urlopen(url)
+
+    #with open(content) as data:
+            
+    #teamdata = json.load(data)
+    #content.close()
+
+    leafdata = teamdata['teams'][0]
+    gamedata = leafdata['nextGameSchedule']['dates'][0]['games'][0]
+        
+    homescoreNew = gamedata['teams']['away']['score']
+    awayscoreNew = gamedata['teams']['home']['score']
+        
+    score = "Score: %d - %d" % (homescoreNew, awayscoreNew)
+        
+    print score
+
+
+    if (awayscoreNew != awayScore):
+       awayScore = awayscoreNew
+       goal_led.process()
+       print "Pens Scored"
+
+    if (homescoreNew != homeScore):
+       homeScore = homescoreNew
+       goal_led.process()
+       print "Bruins Scored"
+
+
 
 def pollNhlApi():
     homeScore = 1 
-    awayScore = 0
+    awayScore = 3
     url = "http://statsapi.web.nhl.com/api/v1/teams/5?expand=team.schedule.next"
 
     goal_led = GoalLed()
-    goal_led.process()
 
     print "Starting NHL API Poll"
 
     while True:
-        content = urllib2.urlopen(url)
-        teamdata = json.load(content)
-        leafdata = teamdata['teams'][0]
-        gamedata = leafdata['nextGameSchedule']['dates'][0]['games'][0]
-        
-        homescoreNew = gamedata['teams']['away']['score']
-        awayscoreNew = gamedata['teams']['home']['score']
-
-        if (awayscoreNew != awayScore):
-            awayScore = awayscoreNew
-            goal_led.process()
-            print "Pens Scored"
-
-        if (homescoreNew != homeScore):
-            homeScore = homescoreNew
-            goal_led.process()
-            print "Bruins Scored"
-        time.sleep(5)
+        getAndParseData(url, goal_led)
+        time.sleep(3)
 
 
 # Main function
