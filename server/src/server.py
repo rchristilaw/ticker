334e6561a32260a3385fdfe1237a733fe202f3d5
Ryan Christilaw <ryan.christilaw@gmail.com>
2018-03-13 21:49:48 -0400

    Server fix

--- server/src/server.py
+++ server/src/server.py
@@ -26,4 +26,4 @@ def activateGoalLight():
 
 if __name__ == "__main__":
     # ticker.initGame("10") 
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(use_reloader=False, host='0.0.0.0', port=8080)
